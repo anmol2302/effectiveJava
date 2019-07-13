4c5c5634ea0946ab4ed727526f6b0a5a4d5b134d
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-14 00:18:10 +0530

    Chapter 2 builder pattern.

---
+++ src/main/java/chapter2/model/User.java
@@ -0,0 +1,76 @@
+package chapter2.model;
+
+public class User {
+
+    private final String name;
+    private final String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    private final String gender;
+    private final String address;
+
+    public User(UserBuilder userBuilder) {
+
+        name = userBuilder.name;
+        age = userBuilder.age;
+        gender = userBuilder.gender;
+        address = userBuilder.address;
+    }
+
+    public static class UserBuilder {
+
+        private String name;
+        private String age;
+        private String gender;
+        private String address;
+
+
+        public UserBuilder name(String value) {
+            name = value;
+            return this;
+        }
+
+        public UserBuilder age(String value) {
+            age = value;
+            return this;
+        }
+
+        public UserBuilder gender(String value) {
+            gender = value;
+            return this;
+        }
+
+        public UserBuilder address(String value) {
+            address = value;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        User user = new UserBuilder().address("Bangalore").age("22").name("anmol").gender("Male").build();
+        System.out.println(user.getAddress());
+
+
+    }
+}
